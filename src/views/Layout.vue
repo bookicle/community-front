b41e35f83dd9ae5ade5dda04bde8fe74b8bc40c2
bookicle <2528901568@qq.com>
2023-11-29 16:39:00 +0800

    layout 2 roll

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {ref, reactive, getCurrentInstance} from "vue";
+import {getCurrentInstance, onMounted, ref} from "vue";
 import {useRoute, useRouter} from "vue-router";
-const {proxy} = getCurrentInstance()
+
+const { proxy } = getCurrentInstance()
 const router = useRouter();
 const route = useRoute();
 
@@ -28,36 +29,66 @@ const logoInfo = [
   },
 ];
 
+const showHeader = ref(true);
+// 获取滚动条的高度
+const getScrollTop = () =>{
+  return document.documentElement.scrollTop ||
+      window.pageXOffset ||
+      document.body.scrollTop;
+};
+
+const initScroll = () => {
+  let initScroll = getScrollTop();
+  let scrollType = 0;
+
+  window.addEventListener("scroll",()=>{
+    let currentScrollTop = getScrollTop();
+    if(currentScrollTop > initScroll){
+      // 往下滚动
+      scrollType = 1;
+    }else{
+      // 往上滚动
+      scrollType = 0;
+    }
+    initScroll = currentScrollTop;
+    showHeader.value = !(scrollType === 1 && currentScrollTop > 100);
+  })
+}
+
+onMounted(()=>{
+  initScroll();
+})
 </script>
 
 <template>
-<div class="header">
-  <div class="header-content" :style="{width: proxy.globalInfo.bodyWidth+'px'}">
-<!--    logo-->
-    <router-link to="/" class="logo ">
-      <span v-for="item in logoInfo" :style="{color: item.color}" >{{item.letter}}</span>
-    </router-link>
-<!--    板块信息-->
-    <div class="menu-panel"></div>
-<!--    登录注册用户信息-->
-    <div class="user-info-panel">
-      <div class="op-btn">
-        <el-button type="primary" >
-          发帖 <span class="iconfont icon-post"> </span></el-button>
-        <el-button type="primary" >
-          搜索 <span class="iconfont icon-search" > </span> </el-button>
+  <div class="header">
+    <div :style="{ width: proxy.globalInfo.bodyWidth + 'px' }"
+         class="header-content">
+      <!--    logo-->
+      <router-link to="/" class="logo ">
+        <span v-for="item in logoInfo" :style="{ color: item.color }">{{ item.letter }}</span>
+      </router-link>
+      <!--    板块信息-->
+      <div class="menu-panel"></div>
+      <!--    登录注册用户信息-->
+      <div class="user-info-panel">
+        <div class="op-btn">
+          <el-button type="primary">
+            发帖 <span class="iconfont icon-post"> </span></el-button>
+          <el-button type="primary">
+            搜索 <span class="iconfont icon-search"> </span></el-button>
+        </div>
+
+        <el-button-group :style="{ 'margin-left': '20px' }">
+          <el-button type="primary" plain>
+            登录 <span class="iconfont icon-login"></span></el-button>
+          <el-button type="primary" plain>
+            注册 <span class="iconfont icon-register"></span></el-button>
+        </el-button-group>
+
       </div>
-
-      <el-button-group :style="{'margin-left':'20px'}">
-        <el-button type="primary" plain >
-          登录 <span class="iconfont icon-login"></span> </el-button>
-        <el-button type="primary" plain >
-          注册 <span class="iconfont icon-register"></span> </el-button>
-      </el-button-group>
-
     </div>
   </div>
-</div>
 </template>
 
 <style lang="scss" scoped>
@@ -65,6 +96,9 @@ const logoInfo = [
   width: 100%;
   position: fixed;
   box-shadow: 0 2px 6px 0 #ddd;
+  z-index: 1000;
+  background: #fff;
+
   .header-content {
     margin: 0 auto;
     align-content: center;
@@ -86,19 +120,20 @@ const logoInfo = [
       flex: 1;
     }
 
-   .user-info-panel {
+    .user-info-panel {
       width: 300px;
       display: flex;
-     .op-btn{
-       .iconfont{
-         margin-left: 5px;
-       }
-       .el-buttun + .el-button {
-          margin-left: 5px;
-     }
-     }
 
-   }
+      .op-btn {
+        .iconfont {
+          margin-left: 5px;
+        }
+
+        .el-buttun+.el-button {
+          margin-left: 5px;
+        }
+      }
+
+    }
   }
-}
-</style>
+}</style>
